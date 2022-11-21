dde1c938d1444653bc551738386f31addab12985
Yari <narducciyarigmail.com>
2022-11-21 18:12:15 +0100

    fine sviluppo

--- Bonus/js/script.js
+++ Bonus/js/script.js
@@ -1,82 +1,89 @@
-//creare una lista di oggetti
+// CONSEGNA
+/* Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
+Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto. */
+
+/* MILESTONE 0:
+Creare l’array di oggetti con le informazioni fornite. */
 const teamMembers = [
     {
-        "name": "Wayne",
-        "surname":"Barnet",
-        "role":"Founder CEO",
-        "photo":"img\wayne-barnett-founder-ceo.jpg"
+        "nome" : "Wayne Barnett",
+        "ruolo" : "Founder & CEO",
+        "foto" : "img/wayne-barnett-founder-ceo.jpg"
     },
     {
-        "name": "Angelica",
-        "surname":"Caroll",
-        "role":"Chief Editor",
-        "photo":"img\angela-caroll-chief-editor.jpg"
+        "nome" : "Angela Caroll",
+        "ruolo" : "Chief Editor",
+        "foto" : "img/angela-caroll-chief-editor.jpg"
     },
     {
-        "name": "Walter",
-        "surname":"Gordon",
-        "role":"Office Manager",
-        "photo":"img\walter-gordon-office-manager.jpg"
+        "nome" : "Walter Gordon",
+        "ruolo" : "Office Manager",
+        "foto" : "img/walter-gordon-office-manager.jpg"
     },
     {
-        "name": "Angela",
-        "surname":"Lopez",
-        "role":"Social Media Manager",
-        "photo":"img\angela-lopez-social-media-manager.jpg"
+        "nome" : "Angela Lopez",
+        "ruolo" : "Social Media Manager",
+        "foto" : "img/angela-lopez-social-media-manager.jpg"
     },
     {
-        "name": "Scott",
-        "surname":"Estrada",
-        "role":"Developer",
-        "photo":"img\scott-estrada-developer.jpg"
+        "nome" : "Scott Estrada",
+        "ruolo" : "Developer",
+        "foto" : "img/scott-estrada-developer.jpg"
     },
     {
-        "name": "Barbara",
-        "surname":"Ramos",
-        "role":"Graphic Designer",
-        "photo":"img\barbara-ramos-graphic-designer.jpg"
+        "nome" : "Barbara Ramos",
+        "ruolo" : "Graphic Designer",
+        "foto" : "img/barbara-ramos-graphic-designer.jpg"
     },
-]
-// let container = document.getElementById("container");
+];
 
-// console.log(teamMembers);
-// for(let i=0; i < teamMembers.length; i++){
-//     let teamIesimo = teamMembers[i];
-//     let container = document.getElementById("container");
-//     let element = document.createElement("div");
-//     let bottomCards = document.querySelector(".bottom-cards");
-//     // element.classList.add("box");
-//     element.innerHTML = `Name:${teamIesimo.name}   Surname:${teamIesimo.surname}   Role:${teamIesimo.role}    Photo:${teamIesimo.photo}`;
-   
-    
-//     // container.append(element);
+console.log(teamMembers);
 
-   
-    
-   
+/* MILESTONE 1:
+Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto */
+for (let i = 0; i < teamMembers.length; i++){
+
+    let member = teamMembers[i];
+    console.log(member.nome, member.ruolo, member.foto);
+}
+
+
+/* MILESTONE 2:
+Stampare le stesse informazioni su DOM sottoforma di stringhe */
+// const ctnCardMembro = document.querySelector("div.container");
+
+// for (let k = 0; k < teamMembers.length; k++){
+
+//     let membroDom = membriTeam[k];
+//     let ctnInfoMembro = createElement("div", "ctn_info_membro");
+//     ctnInfoMembro.innerHTML = `Nome: ${membroDom.nome}, Ruolo: ${membroDom.ruolo}, Foto: ${membroDom.foto}.`;
+//     ctnCardMembro.appendChild(ctnInfoMembro);
 // }
+
+
+// richiamo il container il cui inserire le card dei membri
 const ctnCardMembro = document.querySelector("section.row");
 
 // creo il ciclo for con cui inserire le informazioni degli oggetti
-for (let k = 0; k < teamMembers.length; k++) {
+for (let k = 0; k < membriTeam.length; k++) {
     
-    let membroDom = teamMembers[k];
+    let membroDom = membriTeam[k];
 
     let ctnInfoMembro = createElement("div", "slot");
 
     let fotoMember = createElement("img", "img_member");
     fotoMember.src = membroDom.foto;
-    ctnInfoMembro.append(fotoMember);
+    ctnInfoMembro.appendChild(fotoMember);
 
     let nameMember = createElement("div", "name_member");
     nameMember.innerHTML = membroDom.nome;
-    ctnInfoMembro.append(nameMember);
+    ctnInfoMembro.appendChild(nameMember);
 
     let roleMember = createElement("div", "role_member");
     roleMember.innerHTML = membroDom.ruolo;
-    ctnInfoMembro.append(roleMember);
+    ctnInfoMembro.appendChild(roleMember);
 
-    ctnCardMembro.append(ctnInfoMembro);
+    ctnCardMembro.appendChild(ctnInfoMembro);
 }
 
 // FUNZIONI
